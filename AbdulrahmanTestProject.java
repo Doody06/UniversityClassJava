b0b0eeae498a6a11b5e0391626825b527f2381f8
Doody06 <Abdelrahmanschool06@gmail.com>
2025-01-07 16:45:19 +0300

    Update AbdulrahmanTestProject.java

--- AbdulrahmanTestProject.java
+++ AbdulrahmanTestProject.java
@@ -7,3 +7,55 @@ public class AbdulrahmanTestProject {
    }
 
 }
+
+/** Python > Java
+exercises from book chapter 1
+1.3
+class Main{
+        
+   public static void main(String[] args) {
+       System.out.println("   J     A    V     V     A");
+       System.out.println("   J    A A    V   V     A A");
+       System.out.println("J  J   AAAAA    V V     AAAAA");
+       System.out.println(" JJ   A     A    V     A      A");
+   }
+
+}
+NEED HELP IN THIS ONE
+1.11
+class Main{
+    public static void main(String[] args) {
+        System.out.println(312032486 + (365.0*3600.0)/(7.0) + (365.0 * 3600.0)/(45.0) - (365.0 * 3600.0)/(13.0) );
+        
+        System.out.println(312032486 + (2*365.0*3600.0)/(7.0) + (2*365.0 * 3600.0)/(45.0) - (2*365.0 * 3600.0)/(13.0) );
+        
+        System.out.println(312032486 + (3*365.0*3600.0)/(7.0) + (3*365.0 * 3600.0)/(45.0) - (3*365.0 * 3600.0)/(13.0) );
+        
+        System.out.println(312032486 + (4*365.0*3600.0)/(7.0) + (4*365.0 * 3600.0)/(45.0) - (4*365.0 * 3600.0)/(13.0) );
+        
+        System.out.println(312032486 + (5*365.0*3600.0)/(7.0) + (5*365.0 * 3600.0)/(45.0) - (5*365.0 * 3600.0)/(13.0) );
+        
+
+    }
+}
+1.12
+class Main{
+    public static void main(String[] args) {
+        System.out.println((24*1.6) / (1 + 40.0/60) + "Km / h");
+    }
+}
+1.13
+'''
+Using Cramer's Rule to solve for x and y :
+3.4x + 50.2y == 44.5
+2.1x + 0.55y == 5.9
+'''
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("x = " + (44.5*0.55 - 50.2*5.9) / (3.4*0.55 - 50.2*2.1));
+        System.out.println("y = " + (3.4*5.9 - 44.5*2.1) / (3.4*0.55 - 50.2*2.1));
+        
+    }
+}
+*/
