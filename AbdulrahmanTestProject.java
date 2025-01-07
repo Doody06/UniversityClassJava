23ce474387890681bdb9fcfeb409d004e85bb039
Doody06 <Abdelrahmanschool06@gmail.com>
2025-01-07 11:01:19 +0300

    Add files via upload

---
+++ AbdulrahmanTestProject.java
@@ -0,0 +1,9 @@
+package abdulrahmantestproject;
+
+public class AbdulrahmanTestProject {
+        
+   public static void main(String[] args) {
+       System.out.println("Test");
+   }
+
+}
---
+++ ComputeExpression.java
@@ -0,0 +1,10 @@
+// last accessed Tuesday 1/7/2025
+// Evaluate a simple arithmetic expression
+package abdulrahmantestproject;
+
+public class ComputeExpression {
+    public static void main(String[] args) {
+        System.out.println((10.5+2*3)/(45-3.5));
+
+    }
+}
