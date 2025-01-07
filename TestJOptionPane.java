c237da11b05561d7357f0498bdc0e7a95c69d311
Doody06 <Abdelrahmanschool06@gmail.com>
2025-01-07 11:40:25 +0300

    Add files via upload

---
+++ TestJOptionPane.java
@@ -0,0 +1,17 @@
+/*
+Last accessed 1/7/2025
+Javax test 
+ */
+package abdulrahmantestproject;
+
+import javax.swing.JOptionPane;
+
+public class TestJOptionPane {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,
+                                      "Welcome to Java!",
+                                      "Display Message",
+                                      JOptionPane.INFORMATION_MESSAGE);
+    }
+    
+}
