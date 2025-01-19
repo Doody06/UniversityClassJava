9e04e24ecd34e8a4a21025bad9562806f9b6d8cb
Doody06 <Abdelrahmanschool06@gmail.com>
2025-01-19 11:09:40 +0300

    Update Chapter2_exercises.java

--- Chapter2_exercises.java
+++ Chapter2_exercises.java
@@ -4,20 +4,7 @@ package pkg224110470_chapter2;
 import java.util.Scanner;
 
 public class Chapter2_exercises {
-        public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
-            System.out.print("Enter three points for a triangle: ");
-            
-            double firstPoint = input.nextDouble();
-            double secondPoint = input.nextDouble();
-            double thirdPoint = input.nextDouble();
-            double fourthPoint = input.nextDouble();
-            double fiftPoint = input.nextDouble();
-            double sixthPoint = input.nextDouble();
-            
 
-            
-        }
 }
 /*
   public static void main(String[] args) {
@@ -36,5 +23,20 @@ public class Chapter2_exercises {
         CurrentMonth = (saving + CurrentMonth) * INTEREST_RATE;
 
         System.out.prdoubleln("after the sixth month, the account value is $" + CurrentMonth);
-           
+
+        }
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter three points for a triangle: ");
+            
+            double firstPoint = input.nextDouble();
+            double secondPoint = input.nextDouble();
+            double thirdPoint = input.nextDouble();
+            double fourthPoint = input.nextDouble();
+            double fiftPoint = input.nextDouble();
+            double sixthPoint = input.nextDouble();
+            
+
+            
+        }
 */
